4087dabf114e5e873cf260a391d01f3a776b141d
r0lyi <rsilvestre@elpuig.xeill.net>
2025-04-02 17:08:17 +0200

    Inicio

---
+++ src/test/java/net/shopping/shopCart/ShopCartApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shopping.shopCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
